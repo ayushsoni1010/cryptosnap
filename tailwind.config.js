49d4d1266332026d2bbae613bbfee9dfcc5cd68d
Ayush Soni <ayushsoni1010.work@gmail.com>
2022-12-02 16:41:35 +0530

    update(config): tailwind configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#5A5F7D",
+        secondary: "#ECF0FF",
+        danger: "#FF4D4D",
+        success: "#00FFA3",
+      },
+      fontFamily: {
+        "assets-tomorrow": ["Tomorrow", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
